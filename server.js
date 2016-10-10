d1f7ddc13e78442aa49445eb36ee32108502a90d
Dawid Dyrcz <dyrcu15@gmail.com>
2016-10-10 21:36:09 +0200

    Missing files

--- server.js
+++ server.js
@@ -1,2 +1,37 @@
 var sql = require('mssql');
 
+var config = {
+    // user: '',
+    // password: '',
+    server: 'localhost\\SQLEXPRESS', 
+    database: 'Facebook',    
+    stream: true
+}
+
+
+sql.connect(config, (err) => {
+
+    if(err) throw err;
+
+    console.log('connected to sql!');
+    var request = new sql.Request();     
+
+    request.query('select * from users');
+
+    request.on('row', (data) =>{        
+        console.log(data);
+    });
+
+    request.on('error', (err) => {
+        throw err;
+    });
+
+    request.on('done', (affected) => {
+
+    });    
+
+});
+
+sql.on('error', (err) => {
+    throw err;
+});
