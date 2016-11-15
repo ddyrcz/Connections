22b5065f672fb5b9a72cb89984a23165dfe6ac2b
Dawid Dyrcz <dyrcu15@gmail.com>
2016-11-15 22:52:20 +0100

    Tests creating a post

--- test/user.js
+++ test/user.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 let User = require('../models').User;
+let Post = require('../models').Post;
 
 
 let chai = require('chai');
@@ -11,8 +12,9 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('Users', () => {
-    beforeEach((done) => {
+    before((done) => {
         User.destroy({ where: {} });
+        Post.destroy({ where: {} });
         done();
     });
 
@@ -30,6 +32,8 @@ describe('Users', () => {
     });
 
     describe('Creates a new user', () => {
+
+        var idUser;
         it('it should create a new user', (done) => {
             let user = {
                 name: 'Michal'
@@ -40,9 +44,45 @@ describe('Users', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('name').eql('Michal')
+                    res.body.should.have.property('id');
+                    res.body.should.have.property('name').eql('Michal');
+                    idUser = res.body.id;
                     done();
                 });
+        });
+
+
+        it('it should get one user', (done) => {
+            chai.request(server)
+                .get('/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(1);
+                    done();
+                })
+        });
+
+        describe('The created user makes a post', () => {
+            it('it should tell the new user to make a post', (done) => {
+
+                let post = {
+                    'content': 'Hello every one!',
+                    'userId': idUser
+                }
+
+                chai.request(server)
+                    .post('/posts')
+                    .send(post)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('content').eql('Hello every one!');
+                        res.body.should.have.property('userId').eql(idUser);
+                        done();
+                    });
+
+            })
         });
     });
 
